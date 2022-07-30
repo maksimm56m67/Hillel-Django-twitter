3f6fd268835977c3b4af999aab538bdfd1fab8f6
GypsyQueen <59336564+maksimm56m67@users.noreply.github.com>
2022-07-30 12:32:32 +0300

    Update urls.py

--- twitter/urls.py
+++ twitter/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('twits/', include('twits.urls')), # http://127.0.0.1:8000/twits
+    path('', include('twits.urls')), # http://127.0.0.1:8000/
     path('admin/', admin.site.urls), # http://127.0.0.1:8000/admin
     path('authorizations/', include('authorizations.urls')), # http://127.0.0.1:8000/authorizations
 ]
